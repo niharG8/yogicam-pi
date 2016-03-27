a955916e4ce6f197ca9082036686876dc7feb0cb
Nihar Gandhi <niharG8@gmail.com>
2016-03-27 00:03:40 -0700

    slight tweak to add authentication to mjpg_streamer web interface

--- src/usr/local/bin/start_yogicam.sh
+++ src/usr/local/bin/start_yogicam.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 killall mjpg_streamer
-mjpg_streamer -i "/usr/local/lib/input_uvc.so -d /dev/video0" -o "/usr/local/lib/output_http.so -p 8080 -w /var/www" &
+mjpg_streamer -i "/usr/local/lib/input_uvc.so -d /dev/video0" -o "/usr/local/lib/output_http.so -p 8080 -w /var/www -c yogi:bear" &
